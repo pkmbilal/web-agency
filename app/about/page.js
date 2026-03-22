e4ff6fb2272e17cafd38b541ccfe90e30b1b33f2
Mohammed Bilal <bilal00717@gmail.com>
2026-03-22 12:24:20 +0300

    Remove Navbar and Footer imports from About page

--- app/about/page.js
+++ app/about/page.js
@@ -3,8 +3,6 @@ import AboutIntro from "@/components/about/about-intro";
 import AboutHighlights from "@/components/about/about-highlights";
 import AboutProcess from "@/components/about/about-process";
 import AboutCta from "@/components/about/about-cta";
-import Navbar from "@/components/Navbar";
-import Footer from "@/components/Footer";
 import StatsCounter from "@/components/about/stats-counter";
 
 export default function AboutPage() {
